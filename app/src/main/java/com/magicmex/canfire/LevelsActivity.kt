e9a0d7cc863994c71632722b8df439cf186465a2
Vlad <vladandryschenko98@gmail.com>
2024-08-12 18:52:12 +0300

    UI LevelsActivity: add resources, button_level_first/button_level_second/button_level_three, text_exit, set screenOrientation

---
+++ app/src/main/java/com/magicmex/canfire/LevelsActivity.kt
@@ -0,0 +1,11 @@
+package com.magicmex.canfire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LevelsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_levels)
+    }
+}
