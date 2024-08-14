7c1c41ca685d4f3baf5a82b1730881462e013845
Vlad <vladandryschenko98@gmail.com>
2024-08-14 21:26:31 +0300

    MainActivity: add methods getDisplayMetrics, navigateToWelcomeAndLoadingBar, onBackPressed

--- app/src/main/java/com/magicmex/canfire/MainActivity.kt
+++ app/src/main/java/com/magicmex/canfire/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.magicmex.canfire
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ValueAnimator
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.magicmex.canfire.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        this.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        navigateToWelcomeAndLoadingBar()
+    }
+
+    private fun getDisplayMetrics(): Int {
+        val displayMetrics = resources.displayMetrics
+        displayMetrics.widthPixels
+        val progressBarWidth = (250 * displayMetrics.density).toInt()
+        return progressBarWidth - 10
+    }
+
+    private fun navigateToWelcomeAndLoadingBar() {
+        val maxAnimationWidth = getDisplayMetrics()
+        val layoutParams = binding.loadingBar.loadingLine.layoutParams
+
+        val animation = ValueAnimator.ofInt(10, maxAnimationWidth).apply {
+            this.duration = 3000L
+            addUpdateListener {
+                layoutParams.width = it.animatedValue as Int
+                binding.loadingBar.loadingLine.layoutParams = layoutParams
+            }
+        }
+
+        animation.start()
+
+        startActivity(Intent(this@MainActivity, WelcomeActivity::class.java))
+        finish()
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
     }
 }
