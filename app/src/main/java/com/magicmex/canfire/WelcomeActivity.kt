9db1c6063cc9271265db3fc0741734085ba3fc56
Vlad <vladandryschenko98@gmail.com>
2024-08-14 21:43:40 +0300

    WelcomeActivity: add scale_animation, methods navigateToMenu

--- app/src/main/java/com/magicmex/canfire/WelcomeActivity.kt
+++ app/src/main/java/com/magicmex/canfire/WelcomeActivity.kt
@@ -1,11 +1,33 @@
 package com.magicmex.canfire
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.magicmex.canfire.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityWelcomeBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(binding.root)
+        this.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        navigateToMenu()
+    }
+
+    private fun navigateToMenu() {
+        binding.buttonNext.setOnClickListener {
+            it.startAnimation(AnimationUtils.loadAnimation(this, R.anim.scale_animation))
+            startActivity(Intent(this@WelcomeActivity, MenuActivity::class.java))
+        }
+    }
+
+    @Deprecated(
+        "Deprecated in Java",
+        ReplaceWith("super.onBackPressed()", "androidx.appcompat.app.AppCompatActivity")
+    )
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
