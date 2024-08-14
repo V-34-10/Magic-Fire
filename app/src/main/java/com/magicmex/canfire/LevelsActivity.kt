0578f9668ef0770954f7c1646aa9bdb032092994
Vlad <vladandryschenko98@gmail.com>
2024-08-14 22:31:01 +0300

    LevelsActivity: add methods choiceLevelsGameButton, setupLevelsGameButton, onBackPressed

--- app/src/main/java/com/magicmex/canfire/LevelsActivity.kt
+++ app/src/main/java/com/magicmex/canfire/LevelsActivity.kt
@@ -1,11 +1,48 @@
 package com.magicmex.canfire
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.magicmex.canfire.databinding.ActivityLevelsBinding
 
 class LevelsActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityLevelsBinding.inflate(layoutInflater) }
+    private lateinit var preferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_levels)
+        setContentView(binding.root)
+        this.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        preferences = getSharedPreferences("MagicMexicanFirePref", MODE_PRIVATE)
+        choiceLevelsGameButton()
+    }
+
+    private fun choiceLevelsGameButton() {
+        val animationButton = AnimationUtils.loadAnimation(this, R.anim.scale_animation)
+
+        binding.apply {
+            setupLevelsGameButton(buttonLevelFirst, R.string.button_level_first, animationButton)
+            setupLevelsGameButton(buttonLevelSecond, R.string.button_level_second, animationButton)
+            setupLevelsGameButton(buttonLevelThree, R.string.button_level_three, animationButton)
+        }
+    }
+
+    private fun setupLevelsGameButton(button: View, gameNameResId: Int, animation: Animation) {
+        button.setOnClickListener {
+            it.startAnimation(animation)
+            preferences.edit().putString("LevelGame", getString(gameNameResId)).apply()
+            startActivity(Intent(this@LevelsActivity, SceneActivity::class.java))
+        }
+    }
+
+    @Deprecated(
+        "Deprecated in Java",
+        ReplaceWith("super.onBackPressed()", "androidx.appcompat.app.AppCompatActivity")
+    )
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
