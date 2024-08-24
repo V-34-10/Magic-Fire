3336cd1d2d4484e37c01e53c47cd0acd552f7d4c
Vlad <vladandryschenko98@gmail.com>
2024-08-24 22:45:01 +0300

    SceneActivity: add initGameFragment, createFragmentGames, replaceFragment, onBackPressed

--- app/src/main/java/com/magicmex/canfire/view/games/SceneActivity.kt
+++ app/src/main/java/com/magicmex/canfire/view/games/SceneActivity.kt
@@ -1,12 +1,75 @@
 package com.magicmex.canfire.view.games
 
+import android.content.Intent
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
 import com.magicmex.canfire.R
+import com.magicmex.canfire.databinding.ActivitySceneBinding
+import com.magicmex.canfire.view.games.findgame.FindPairGameFragment
+import com.magicmex.canfire.view.games.kenogame.KenoGameFragment
+import com.magicmex.canfire.view.level.LevelsActivity
 
 class SceneActivity : AppCompatActivity() {
+    private val binding by lazy { ActivitySceneBinding.inflate(layoutInflater) }
+    private val preferences by lazy {
+        getSharedPreferences("MagicMexicanFirePref", MODE_PRIVATE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scene)
+        setContentView(binding.root)
+        this.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        initGameFragment()
+    }
+
+    private fun initGameFragment() = replaceFragment(
+        createFragmentGames(
+            preferences.getString(
+                "GameName",
+                getString(R.string.button_game_first)
+            )
+        ), R.id.container_games
+    )
+
+    private fun createFragmentGames(game: String?): Fragment {
+        return when (game) {
+            getString(R.string.button_game_second) -> FindPairGameFragment()
+            else -> KenoGameFragment()
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment, containerId: Int) {
+        supportFragmentManager.commit {
+            replace(containerId, fragment)
+            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (!handleBackStack()) {
+            navigateToMenuActivity()
+        }
+    }
+
+    private fun handleBackStack(): Boolean {
+        return if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+            true
+        } else {
+            false
+        }
+    }
+
+    private fun navigateToMenuActivity() {
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        startActivity(Intent(this, LevelsActivity::class.java))
+        finish()
     }
 }
