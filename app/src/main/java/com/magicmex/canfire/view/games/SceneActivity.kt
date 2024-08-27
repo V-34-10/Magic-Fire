c641204f4bde766e36bd582ebd67602dcfa5b155
Vlad <vladandryschenko98@gmail.com>
2024-08-27 15:14:43 +0300

    SceneActivity: fix rubbish code

--- app/src/main/java/com/magicmex/canfire/view/games/SceneActivity.kt
+++ app/src/main/java/com/magicmex/canfire/view/games/SceneActivity.kt
@@ -2,9 +2,9 @@ package com.magicmex.canfire.view.games
 
 import android.content.Intent
 import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
