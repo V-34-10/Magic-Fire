3e0668650656683383cb601420c166ec14f91630
Vlad <vladandryschenko98@gmail.com>
2024-08-27 20:04:47 +0300

    HighScoreKenoManager: add statsHighScoreKeno, saveStatsScoreKenoGame, loadScoreKenoGame

---
+++ app/src/main/java/com/magicmex/canfire/view/games/kenogame/dialog/HighScoreKenoManager.kt
@@ -0,0 +1,31 @@
+package com.magicmex.canfire.view.games.kenogame.dialog
+
+import android.content.SharedPreferences
+import com.magicmex.canfire.model.HighScoreKeno
+
+object HighScoreKenoManager {
+
+    val statsHighScoreKeno = mutableMapOf(
+        "Level 1" to HighScoreKeno(),
+        "Level 2" to HighScoreKeno(),
+        "Level 3" to HighScoreKeno()
+    )
+
+    fun saveStatsScoreKenoGame(preferences: SharedPreferences) {
+        val editor = preferences.edit()
+        for ((level, stats) in statsHighScoreKeno) {
+            editor.putInt("${level}_gamesPlayed", stats.countAllGamesPlayed)
+            editor.putInt("${level}_wins", stats.countWins)
+            editor.putInt("${level}_losses", stats.countLosses)
+        }
+        editor.apply()
+    }
+
+    fun loadScoreKenoGame(sharedPref: SharedPreferences) {
+        for ((level, stats) in statsHighScoreKeno) {
+            stats.countAllGamesPlayed = sharedPref.getInt("${level}_gamesPlayed", 0)
+            stats.countWins = sharedPref.getInt("${level}_wins", 0)
+            stats.countLosses = sharedPref.getInt("${level}_losses", 0)
+        }
+    }
+}
