4164d8eb83e0c910ad07ad060c8d1260ea55c08a
Vlad <vladandryschenko98@gmail.com>
2024-08-28 12:27:21 +0300

    NavigationManager: add setNavigationBarVisibility, isNavigationBarHiddenRequired

---
+++ app/src/main/java/com/magicmex/canfire/view/navigation/NavigationManager.kt
@@ -0,0 +1,17 @@
+package com.magicmex.canfire.view.navigation
+
+import android.app.Activity
+import android.content.res.Configuration
+import android.os.Build
+import android.view.View
+
+object NavigationManager {
+    private fun isNavigationBarHiddenRequired(activity: Activity): Boolean =
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && activity.resources.configuration.navigation == Configuration.NAVIGATION_NONAV
+
+    fun setNavigationBarVisibility(activity: Activity) {
+        if (isNavigationBarHiddenRequired(activity)) {
+            activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+    }
+}
