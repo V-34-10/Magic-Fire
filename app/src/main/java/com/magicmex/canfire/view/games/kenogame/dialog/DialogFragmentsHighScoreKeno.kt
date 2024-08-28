2c635f854b01927cb76204dc1048dbf095f770be
Vlad <vladandryschenko98@gmail.com>
2024-08-28 19:44:04 +0300

    DialogFragmentsHighScoreKeno: fix remove rubbish code

--- app/src/main/java/com/magicmex/canfire/view/games/kenogame/dialog/DialogFragmentsHighScoreKeno.kt
+++ app/src/main/java/com/magicmex/canfire/view/games/kenogame/dialog/DialogFragmentsHighScoreKeno.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Context
 import android.content.SharedPreferences
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.view.View
 import android.view.Window
 import android.widget.TextView
