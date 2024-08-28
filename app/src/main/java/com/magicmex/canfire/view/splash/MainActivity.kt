e44cba0f55cd1c9c270207339bb02169aa8819f0
Vlad <vladandryschenko98@gmail.com>
2024-08-28 21:30:44 +0300

    Splash: add checkNavigateToPrivacy, checkStateEthernet, initBorderBanner

--- app/src/main/java/com/magicmex/canfire/view/splash/MainActivity.kt
+++ app/src/main/java/com/magicmex/canfire/view/splash/MainActivity.kt
@@ -1,23 +1,34 @@
 package com.magicmex.canfire.view.splash
 
 import android.animation.ValueAnimator
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.magicmex.canfire.databinding.ActivityMainBinding
+import com.magicmex.canfire.view.border.BorderBanner
+import com.magicmex.canfire.view.games.findgame.manager.GameSettings
 import com.magicmex.canfire.view.navigation.NavigationManager
+import com.magicmex.canfire.view.privacy.PrivacyActivity
 import com.magicmex.canfire.view.welcome.WelcomeActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var preferencesApp: SharedPreferences
+    private lateinit var borderBanner: BorderBanner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         NavigationManager.setNavigationBarVisibility(this)
-        navigateToWelcome()
+        preferencesApp = GameSettings.getPreference(this)
+        borderBanner = BorderBanner(this, binding)
+        navigateTo()
     }
 
     private fun getDisplayMetrics(): Int {
@@ -27,12 +38,51 @@ class MainActivity : AppCompatActivity() {
         return progressBarWidth - 10
     }
 
-    private fun navigateToWelcome() {
+    private fun navigateTo() {
         loadingBar()
         lifecycleScope.launch {
             delay(3000L)
+            if (!checkStateEthernet()) {
+                checkNavigateToPrivacy()
+            } else {
+                initBorderBanner()
+            }
+        }
+    }
+
+    private fun checkNavigateToPrivacy() {
+        if (!preferencesApp.getBoolean("PrivacyStatus", false)) {
+            startActivity(Intent(this@MainActivity, PrivacyActivity::class.java))
+            preferencesApp.edit().putBoolean("PrivacyStatus", true).apply()
+        } else {
             startActivity(Intent(this@MainActivity, WelcomeActivity::class.java))
-            finish()
+        }
+        finish()
+    }
+
+    @SuppressLint("ObsoleteSdkInt", "ServiceCast")
+    private fun checkStateEthernet(): Boolean {
+        val activeNetworkInfo =
+            (getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo
+        return activeNetworkInfo != null && (activeNetworkInfo.isConnected || activeNetworkInfo.isConnectedOrConnecting)
+    }
+
+    private fun initBorderBanner() {
+        val hasUserData =
+            preferencesApp.contains("cookies") && preferencesApp.contains("userAgent")
+        if (hasUserData) {
+            val cookies = preferencesApp.getString("cookies", "")
+            val userAgent = preferencesApp.getString("userAgent", "")
+            val actionUrl = preferencesApp.getString("actionUrl", "")
+            val sourceUrl = preferencesApp.getString("sourceUrl", "")
+            borderBanner.loadImage(
+                sourceUrl.toString(),
+                actionUrl.toString(),
+                cookies,
+                userAgent
+            )
+        } else {
+            borderBanner.fetchInterstitialData()
         }
     }
 
@@ -55,5 +105,10 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
         finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        borderBanner.cancel()
     }
 }
