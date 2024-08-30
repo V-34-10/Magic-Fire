6cb838ab819e69dd826a0e4184c5cd8ed063b6a8
Vlad <vladandryschenko98@gmail.com>
2024-08-30 14:20:36 +0300

    MusicVolumeManager: add setMusicVolume, offMusicVolume

---
+++ app/src/main/java/com/magicmex/canfire/view/settings/music/MusicVolumeManager.kt
@@ -0,0 +1,16 @@
+package com.magicmex.canfire.view.settings.music
+
+import android.content.Context
+import android.media.AudioManager
+
+class MusicVolumeManager(context: Context) {
+    private val managerMusic by lazy { context.getSystemService(Context.AUDIO_SERVICE) as AudioManager }
+    private var defaultMusicVolume: Int = 25
+    fun setMusicVolume() =
+        managerMusic.setStreamVolume(AudioManager.STREAM_MUSIC, defaultMusicVolume, 0)
+
+    fun offMusicVolume() {
+        defaultMusicVolume = managerMusic.getStreamVolume(AudioManager.STREAM_MUSIC)
+        managerMusic.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0)
+    }
+}
