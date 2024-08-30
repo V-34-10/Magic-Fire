60c9ffa86c3bed6807efbea9140e619e941d769e
Vlad <vladandryschenko98@gmail.com>
2024-08-30 14:19:42 +0300

    PreferenceManager: add initSettings, setSettingsMusicTrue, setSettingsVibroTrue, setSettingsMusicFalse, setSettingsVibroFalse

--- app/src/main/java/com/magicmex/canfire/utils/preference/PreferenceManager.kt
+++ app/src/main/java/com/magicmex/canfire/utils/preference/PreferenceManager.kt
@@ -5,15 +5,35 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 
 object PreferenceManager {
-    private lateinit var preferences: SharedPreferences
-
     var selectedLevel: String = ""
         private set
 
-    fun init(context: Context) {
-        preferences = getPreference(context)
-        selectedLevel = preferences.getString("LevelGame", "") ?: ""
+    var vibroStatus: Boolean = false
+        private set
+
+    var musicStatus: Boolean = false
+        private set
+
+    fun initLevelGame(context: Context) {
+        selectedLevel = getPreference(context).getString("LevelGame", "") ?: ""
     }
+
+    fun initSettings(context: Context) {
+        vibroStatus = getPreference(context).getBoolean("vibroStatus", false)
+        musicStatus = getPreference(context).getBoolean("musicStatus", false)
+    }
+
+    fun setSettingsMusicTrue(context: Context) =
+        getPreference(context).edit().putBoolean("musicStatus", true).apply()
+
+    fun setSettingsVibroTrue(context: Context) =
+        getPreference(context).edit().putBoolean("vibroStatus", true).apply()
+
+    fun setSettingsMusicFalse(context: Context) =
+        getPreference(context).edit().putBoolean("musicStatus", false).apply()
+
+    fun setSettingsVibroFalse(context: Context) =
+        getPreference(context).edit().putBoolean("vibroStatus", false).apply()
 
     fun getPreference(context: Context): SharedPreferences =
         context.getSharedPreferences("MagicMexicanFirePref", AppCompatActivity.MODE_PRIVATE)
