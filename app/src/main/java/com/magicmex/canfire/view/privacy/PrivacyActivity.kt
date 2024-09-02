fed34731fb24aa6a4b7d4769a9020d47a07f6584
Vlad <vladandryschenko98@gmail.com>
2024-09-02 15:46:59 +0300

    PrivacyActivity: add preferencesApp accept click button

--- app/src/main/java/com/magicmex/canfire/view/privacy/PrivacyActivity.kt
+++ app/src/main/java/com/magicmex/canfire/view/privacy/PrivacyActivity.kt
@@ -1,6 +1,7 @@
 package com.magicmex.canfire.view.privacy
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,14 +9,17 @@ import com.magicmex.canfire.R
 import com.magicmex.canfire.databinding.ActivityPrivacyBinding
 import com.magicmex.canfire.utils.animation.AnimationManager.setAnimationClickButton
 import com.magicmex.canfire.utils.navigation.NavigationManager
+import com.magicmex.canfire.utils.preference.PreferenceManager
 import com.magicmex.canfire.view.menu.MenuActivity
 
 class PrivacyActivity : AppCompatActivity() {
     private val binding by lazy { ActivityPrivacyBinding.inflate(layoutInflater) }
+    private lateinit var preferencesApp: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         NavigationManager.setNavigationBarVisibility(this)
+        preferencesApp = PreferenceManager.getPreference(this)
         acceptPrivacyPolicy()
     }
 
@@ -23,6 +27,7 @@ class PrivacyActivity : AppCompatActivity() {
         binding.apply {
             btnAccept.setOnClickListener {
                 it.startAnimation(setAnimationClickButton(this@PrivacyActivity))
+                preferencesApp.edit().putBoolean("PrivacyStatus", true).apply()
                 startActivity(Intent(this@PrivacyActivity, MenuActivity::class.java))
                 finish()
             }
