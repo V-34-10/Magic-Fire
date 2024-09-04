fcc0d7633a1c7e588d639a06b7cd436caa2d086d
Vlad <vladandryschenko98@gmail.com>
2024-09-04 19:29:17 +0300

    App: change Appodeal appKey

--- app/src/main/java/com/magicmex/canfiree/App.kt
+++ app/src/main/java/com/magicmex/canfiree/App.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import com.appodeal.ads.Appodeal
 
 class App : Application() {
-    private val appKey = "9a6df8362de4a5e6deb688ee2110a3e380e788aa834c4170"
+    private val appKey = "54a2c350801372881d0ff96e40b2536f964559b5526ed38a"
     override fun onCreate() {
         super.onCreate()
         Appodeal.initialize(this, appKey, Appodeal.INTERSTITIAL)
