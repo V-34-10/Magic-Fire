ed5201039eee7f6941f5ec94d96568e90175d3f5
Vlad <vladandryschenko98@gmail.com>
2024-09-04 13:18:17 +0300

    Project: fix config

--- app/src/main/java/com/magicmex/canfire/view/splash/MainActivity.kt
+++ app/src/main/java/com/magicmex/canfire/view/splash/MainActivity.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
