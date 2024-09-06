afff4d8416e01f8c0ce37e26d322a57f110e4115
Vlad <vladandryschenko98@gmail.com>
2024-09-06 12:02:05 +0300

    ItemManager: optimization: getImagesSwitchLevel, getImages; add data class LevelConfig

--- app/src/main/java/com/magicmex/canfiree/view/games/findgame/manager/ItemManager.kt
+++ app/src/main/java/com/magicmex/canfiree/view/games/findgame/manager/ItemManager.kt
@@ -2,64 +2,50 @@ package com.magicmex.canfiree.view.games.findgame.manager
 
 import com.magicmex.canfiree.R
 
+data class LevelConfig(
+    val images: List<Int>,
+    val repeatTimes: Int,
+    val extraElements: Int
+)
+
 object ItemManager {
 
+    private val levelConfigurations = mapOf(
+        "Level 1" to LevelConfig(
+            images = listOf(
+                R.drawable.find_1,
+                R.drawable.find_2,
+                R.drawable.find_3,
+                R.drawable.find_4
+            ),
+            repeatTimes = 1,
+            extraElements = 4
+        ),
+        "Level 2" to LevelConfig(
+            images = listOf(
+                R.drawable.find_1, R.drawable.find_2, R.drawable.find_3, R.drawable.find_4,
+                R.drawable.find_5, R.drawable.find__6, R.drawable.find_7, R.drawable.find_8
+            ),
+            repeatTimes = 2,
+            extraElements = 3
+        ),
+        "Level 3" to LevelConfig(
+            images = listOf(
+                R.drawable.find_1, R.drawable.find_2, R.drawable.find_3, R.drawable.find_4,
+                R.drawable.find_5, R.drawable.find__6, R.drawable.find_7, R.drawable.find_8,
+                R.drawable.find_9, R.drawable.find_10, R.drawable.find_11, R.drawable.find_12
+            ),
+            repeatTimes = 4,
+            extraElements = 4
+        )
+    )
+
     fun getImagesSwitchLevel(level: String): List<Int> {
-        val baseImages = getImages(level)
-        val repetitions = when (level) {
-            "Level 1" -> 1 to 4
-            "Level 2" -> 2 to 3
-            "Level 3" -> 4 to 4
-            else -> 1 to 4
-        }
-        return baseImages.repeatAndAdd(repetitions.first, repetitions.second)
+        val config = levelConfigurations[level] ?: levelConfigurations["Level 1"]!!
+        return config.images.repeatAndAdd(config.repeatTimes, config.extraElements)
     }
 
     private fun List<Int>.repeatAndAdd(repeatTimes: Int, extraElements: Int): List<Int> {
         return this + List(repeatTimes) { this }.flatten() + take(extraElements)
-    }
-
-    private fun getImages(level: String): List<Int> {
-        return when (level) {
-            "Level 1" -> listOf(
-                R.drawable.find_1,
-                R.drawable.find_2,
-                R.drawable.find_3,
-                R.drawable.find_4
-            )
-
-            "Level 2" -> listOf(
-                R.drawable.find_1,
-                R.drawable.find_2,
-                R.drawable.find_3,
-                R.drawable.find_4,
-                R.drawable.find_5,
-                R.drawable.find__6,
-                R.drawable.find_7,
-                R.drawable.find_8
-            )
-
-            "Level 3" -> listOf(
-                R.drawable.find_1,
-                R.drawable.find_2,
-                R.drawable.find_3,
-                R.drawable.find_4,
-                R.drawable.find_5,
-                R.drawable.find__6,
-                R.drawable.find_7,
-                R.drawable.find_8,
-                R.drawable.find_9,
-                R.drawable.find_10,
-                R.drawable.find_11,
-                R.drawable.find_12
-            )
-
-            else -> listOf(
-                R.drawable.find_1,
-                R.drawable.find_2,
-                R.drawable.find_3,
-                R.drawable.find_4
-            )
-        }
     }
 }
